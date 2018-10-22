3886346d3811743c3d5ed1bda0127d00bb2af307
Olga Conesa <olgaconesaboj@gmail.com>
2018-10-22 10:37:55 +0200

    Update Javascript ejercicios practicas/js-playground/src/calificaciones.js

--- Javascript ejercicios practicas/js-playground/src/calificaciones.js
+++ Javascript ejercicios practicas/js-playground/src/calificaciones.js
@@ -5,12 +5,12 @@
 // Dada la calificación de alumnos de una clase en forma de objeto como el siguiente:
 
 const eso2o = {
-    David: 8.25,
+    David: 10,
     Maria: 9.5,
-    Jose: 6.75,
-    Juan: 5.5,
-    Blanca: 7.75,
-    Carmen: 8,
+    Jose:9.7,
+    Juan: 10,
+    Blanca: 6,
+    Carmen: 10,
   };
   
   // Implementa una función que muestre la media de la clase de forma textual, es decir, siguiendo
@@ -22,11 +22,55 @@ const eso2o = {
   // Suficiente = entre 5 y 6
   // Insuficiente = entre 4 y 5
   // Muy deficiente = por debajo de 4
-  
-  function printAverage(classResults) {
-    // Implementation here.
-  }
-  
+
+//Implementamos una función donde sacaremos la media de la suma de todas las notas
+const calcularMedia = function(classResults){
+  //variable donde guardaremos la suma de las notas
+  var resultado = Object.values(classResults);
+  //sacamos la media total
+  return resultado.reduce(function(suma, media){
+    return suma + media;
+  }, 0) / resultado.length;
+}
+
+//implementamos un switch para clasificar la nota media sacada anteriormente
+const getClasificacion = function (media){
+  switch (Math.floor(media)){
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    return "Muy deficiente";
+    case 4:
+    return "Insuficiente";
+    case 5:
+    return "suficiente";
+    case 6:
+    return "Bien";
+    case 7:
+    case 8:
+    return "Notable";
+    case 9:
+    return "Sobresaliente";
+    default:
+    return "Matrícula de Honor";
+  } 
+}
+
+
+  //Función para mostrar la nota en forma textuals
+   const mostrarMedia= function (classResults) {
+
+   console.log(getClasificacion(calcularMedia(classResults)));
+
+  } 
+ 
+  mostrarMedia(eso2o);
+
+
+
+
+
   // TIP: Rompe en tantas funciones auxiliares como necesites.
   // TIP: Utiliza el ejercicio "values" para extraer los valores de un objeto.
   // En Array.prototype también cuentas con otro método que podría serte de utilidad
