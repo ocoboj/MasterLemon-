7d1a1f47ea00d3c1f0883cb059f66340f9b9a299
Olga Conesa <olgaconesaboj@gmail.com>
2018-11-17 10:42:17 +0100

    Webpack
    practica bulding con webpack, primera parte y import

---
+++ ptawebpack/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: [
+        '@babel/polyfill',
+        './students.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+};
