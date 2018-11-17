c1709ee95615d4f4d759e2ad7899602eb5a520ca
Olga Conesa <olgaconesaboj@gmail.com>
2018-11-17 12:38:56 +0100

    Aplicar estilo scss
    Cambiar css por scss y instalación del paquete.

--- ptawebpack/webpack.config.js
+++ ptawebpack/webpack.config.js
@@ -1,11 +1,38 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+
+var path = require('path');
+var basePath = __dirname;
+
 module.exports = {
-    entry: [
-        '@babel/polyfill',
-        './students.js'
-    ],
-    output: {
-        filename: 'bundle.js',
+    entry: {
+        app: './students.js',
+        appStyles: [
+            './mystyles.scss',
+        ],
+        vendor: [
+            'jquery',
+        ],
     },
+    output: {
+        filename: '[name].[chunkhash].js',
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'initial',
+                    name: 'vendor',
+                    test: 'vendor',
+                    enforce: true
+                },
+            }
+        },
+    },
+
+
     module: {
         rules: [
             {
@@ -13,6 +40,35 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // este esta en ./dist
+            template: 'index.html', // este esta en ./
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
